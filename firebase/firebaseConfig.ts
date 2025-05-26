2de7dfbaa5858eab5e72219906d1a4a88f006ef5
Birkti Girma Ayele <60798849+Birktiayele@users.noreply.github.com>
2025-05-25 21:48:32 -0700

    Update firebaseConfig.ts

--- firebase/firebaseConfig.ts
+++ firebase/firebaseConfig.ts
@@ -6,12 +6,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAqRvJ4KtlBth166cPAmWk0uCHwztllM3k",
-  authDomain: "mealplannerapp-32cac.firebaseapp.com",
-  projectId: "mealplannerapp-32cac",
-  storageBucket: "mealplannerapp-32cac.firebasestorage.app",
-  messagingSenderId: "261748729246",
-  appId: "1:261748729246:web:1b4c87bdfd8deb4d56e28f"
+  apiKey: "your-api-key",
+  authDomain: "your-auth-domain",
+  projectId: "your-project-id",
+  storageBucket: "your-storage-bucket",
+  messagingSenderId: "your-messaging-sender-id",
+  appId: "your-app-id",
 };
 
 const app = initializeApp(firebaseConfig);
